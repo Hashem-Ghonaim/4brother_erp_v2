25ad37dc71e491c18d8639e0f94245704bd21876
Hashem-Ghonaim <hashem@example.com>
2026-08-02 03:29:23 +0300

    feat: display error traceback on 500

--- api/index.py
+++ api/index.py
@@ -6,5 +6,14 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from backend.app import app
 
+import traceback
+@app.errorhandler(500)
+def internal_error(e):
+    return "<pre>" + traceback.format_exc() + "</pre>", 500
+
+@app.errorhandler(Exception)
+def unhandled_exception(e):
+    return "<pre>" + traceback.format_exc() + "</pre>", 500
+
 # This is required for Vercel
 # The variable name must be 'app'
